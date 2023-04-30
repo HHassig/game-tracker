4f3829d96d74240934f50291d758f08fb0311802
Harrison Hassig <harrisonhassig@gmail.com>
2023-04-30 23:45:32 +0200

    Okay

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,19 @@
 class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
-  before_action :set_user
 
   def index
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path(@user)
     else
@@ -20,16 +22,19 @@ class UsersController < ApplicationController
   end
 
   def follow
+    @user = User.find(params[:id])
     current_user.send_follow_request_to(@user)
     redirect_to user_path(@user)
   end
 
   def unfollow
+    @user = User.find(params[:id])
     current_user.unfollow(@user)
     redirect_to user_path(@user)
   end
 
   def accept
+    @user = User.find(params[:id])
     current_user.accept_follow_request_of(@user)
   end
 
@@ -40,10 +45,6 @@ class UsersController < ApplicationController
   end
 
   private
-
-  def set_user
-    @user = User.find(params[:id])
-  end
 
   def user_params
     params.require(:user).permit(:username, :photo)
