6a51e555ff1c899ecfcd612d70169be2ec5b19a1
Harrison Hassig <harrisonhassig@gmail.com>
2023-05-01 12:29:29 +0200

    Defualt username overwritten properly

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,14 @@ class User < ApplicationRecord
   followability
   has_one_attached :photo
 
+  def unfollow(user)
+    followerable_relationships.where(followable_id: user.id).destroy_all
+  end
+
+  private
 
   def set_defaults
     at_sign = self.email.index("@").to_i - 1
-    self.username = self.email[0..at_sign]
-  end
-
-  def unfollow(user)
-    followerable_relationships.where(followable_id: user.id).destroy_all
+    self.username ||= self.email[0..at_sign]
   end
 end
