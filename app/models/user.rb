8108a6e12cda8a6ed69a7bcfa080c0e1845b363e
Harrison Hassig <harrisonhassig@gmail.com>
2023-05-01 09:13:36 +0200

    Set default username to email before at sign

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ApplicationRecord
+  after_initialize :set_defaults
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   followability
   has_one_attached :photo
+
+
+  def set_defaults
+    at_sign = self.email.index("@").to_i - 1
+    self.username = self.email[0..at_sign]
+  end
 
   def unfollow(user)
     followerable_relationships.where(followable_id: user.id).destroy_all
