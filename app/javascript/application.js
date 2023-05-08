123812a8234cbb17295a3ce14e02e3ac922710e3
Harrison Hassig <harrisonhassig@gmail.com>
2023-05-08 11:26:30 +0200

    Remove cookie stuff for now

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,4 +2,3 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
-//= require cookies_eu
--- config/initializers/rails_cookie_consent.rb
+++
@@ -1,7 +0,0 @@
-RailsCookieConsent.configure do |config|
-  # check RailsCookieConsent::DEFAULT_CONFIG for options
-  config.cookie_types = [
-    { value: 'necessary', readonly: true, enabled: true, required: true },
-    { value: 'required', readonly: false, enabled: false, required: false }
-  ]
-end
