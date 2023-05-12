32ae2ccf715b144d39db1d98e36f473e97374f46
Harrison Hassig <harrisonhassig@gmail.com>
2023-05-12 12:11:54 +0200

    Fixed averages and more efficient

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,8 +1,7 @@
 class ResultsController < ApplicationController
   def index
     @game = Game.find(params[:game_id])
-    @user = current_user
-    @average = Average.find_by(game: @game, user: current_user)
+    @average = get_average(@game, current_user)
     @results = Result.where(game: @game.id, user: current_user).sort_by(&:edition_int)
   end
 
@@ -16,24 +15,13 @@ class ResultsController < ApplicationController
     @result = Result.new(result_params)
     @result.user = current_user.id
     @result.game = @game.id
-    @average = Average.find_by(game: @game, user: current_user)
     @result.score = @result.get_score(@result.guess)
     @result.edition = @result.get_edition(@result.guess)
     @result.display_score = @result.get_display_score(@result.guess)
     @result.edition_int = @result.get_edition_int(@result.guess)
     unless @result.score.nil?
       @result.save!
-      unless @average.nil?
-        @results = Result.where(game: @game, user: current_user)
-        @sum_score = 0
-        @results.each do |result|
-          @sum_score += result.score.to_i
-        end
-        @average.average = '%.2f' % (@sum_score / @results.size.to_f)
-      else
-        @average = Average.new(game: @game.id, user: current_user.id, average: @result.score)
-      end
-      @average.save!
+      set_average(@game, current_user)
       redirect_to game_result_path(@game, @result), notice: 'Result noted.'
     else
       redirect_to new_game_result_path(@game), notice: 'Result not valid, try again.'
@@ -58,11 +46,11 @@ class ResultsController < ApplicationController
   def update
     @game = Game.find(params[:game_id])
     @result = Result.find(params[:id])
-    @average = Average.find_by(game: @game, user: current_user)
     @result.score = @result.get_score(@result.guess)
     @result.edition = @result.get_edition(@result.guess)
     @result.display_score = @result.get_display_score(@result.guess)
     @result.edition_int = @result.get_edition_int(@result.guess)
+    @average = set_average(@game, current_user)
     if @result.update(result_params)
       redirect_to game_result_path(@game, @result)
     else
@@ -81,5 +69,26 @@ class ResultsController < ApplicationController
 
   def result_params
     params.require(:result).permit(:user, :score, :guess, :game)
+  end
+
+  def set_average(game, user)
+    results = Result.where(game: @game, user: current_user)
+    if Average.find_by(game: game, user: user).nil?
+      avg = Average.create!(game: game, user: user, average: results.first.score)
+    else
+      avg = Average.find_by(game: game, user: user)
+      sum_score = 0
+      results.each do |result|
+        sum_score += result.score.to_i
+      end
+      avg.average = '%.2f' % (sum_score / results.size.to_f)
+      avg.save!
+    end
+    return avg
+  end
+
+  def get_average(game, user)
+    return 0 unless Average.find_by(game: game, user: user).nil?
+    return Average.find_by(game: game, user: user)
   end
 end
