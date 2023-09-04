9b57db5d259ed89c52b1138d5747eb7eb553087e
Harrison Hassig <harrisonhassig@gmail.com>
2023-09-04 15:11:02 +0200

    Index.js2

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,6 +6,3 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
-
-import PortfolioListToggleController from "./portfolio_list_toggle_controller"
-application.register("portfolio-list-toggle", PortfolioListToggleController)
